f5b09bc6f6d015fd39e83af9150725d1a4d1fea6
Aaryan Shah <aaryanshah615@gmail.com>
2023-10-05 22:27:14 +0545

    setting setup

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j8jzc7qp)&=hl7qlvr@)c5!v%))i27w#ef03$&^1h#&26x%ccr'
+SECRET_KEY = '1w#6909cfm+h_d&)g(xswclgvxh-@zb3hbvhb@kq57+_@x$9be'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- manage.py
+++ manage.py
@@ -2,11 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from ecommerce.settings import base
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+    if base.DEBUG:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings.local')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings.production')  
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
