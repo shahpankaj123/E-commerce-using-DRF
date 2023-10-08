05b4eae2c62f62241bd8da2d057d759fdf2e8c31
Aaryan Shah <aaryanshah615@gmail.com>
2023-10-08 19:08:01 +0545

    final stage of project

--- ecommerce/product/admin.py
+++ ecommerce/product/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models import *
 
+class ProductInline(admin.TabularInline):
+    model = ProductLine
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductInline,
+    ]
+
 admin.site.register(Brand)
 admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(ProductLine)
+
 
 # Register your models here.
