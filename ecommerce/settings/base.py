a2659558f71c29fdef3aa33c801982e7ee5b4352
Aaryan Shah <aaryanshah615@gmail.com>
2023-10-15 20:39:21 +0545

    setting

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -5,13 +5,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '1w#6909cfm+h_d&)g(xswclgvxh-@zb3hbvhb@kq57+_@x$9be'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
